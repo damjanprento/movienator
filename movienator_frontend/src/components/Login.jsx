37e1a2eabf033112b972078eda83f0d4349ad0b8
damjanprento <dprentoski48@yahoo.com>
2022-03-14 14:00:07 +0100

    update

--- movienator_frontend/src/components/Login.jsx
+++ movienator_frontend/src/components/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import GoogleLogin from 'react-google-login';
 import { useNavigate } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
-import shareVideo from '../assets/share.mp4';
+import blackHole from '../assets/black-hole-scene.mp4';
 import logo from '../assets/movienator.png';
 
 import { client } from '../client';
@@ -32,7 +32,7 @@ const Login = () => {
     <div className="flex justify-start items-center flex-col h-screen">
         <div className="relative w-full h-full">
           <video 
-            src={shareVideo}
+            src={blackHole}
             type="video/mp4"
             loop
             controls={false}
